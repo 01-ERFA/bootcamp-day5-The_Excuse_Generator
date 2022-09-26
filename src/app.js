422c8fca742dc96e66a81801c4f301aee63c0e44
01-ERFA <atukdibe@gmail.com>
2022-09-26 18:18:01 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+window.onload = () => {
+  console.log("Hello World");
+
+  document.getElementById("my_button").addEventListener("click", random_excuse);
 };
+
+function random_excuse() {
+  let index_random_who = Math.floor(Math.random() * who.length);
+
+  let index_random_what = Math.floor(Math.random() * what.length);
+
+  let index_random_when = Math.floor(Math.random() * when.length);
+
+  let result =
+    who[index_random_who] +
+    " " +
+    what[index_random_what] +
+    " " +
+    when[index_random_when];
+
+  document.getElementById("excuse").innerHTML = result;
+  document.getElementById("my_button").innerHTML = "Repeat";
+}
